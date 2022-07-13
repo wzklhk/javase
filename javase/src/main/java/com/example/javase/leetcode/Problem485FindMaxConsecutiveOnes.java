3953c8583e79effb6043a099bdb66ae058a771b0
ZPAVelocity <velocity120@outlook.com>
2022-07-13 23:31:42 +0800

    leetcode 485 easy

---
+++ javase/src/main/java/com/example/javase/leetcode/Problem485FindMaxConsecutiveOnes.java
@@ -0,0 +1,28 @@
+package com.example.javase.leetcode;
+
+public class Problem485FindMaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsecutiveOnes = 0;
+        int current = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                current += 1;
+                if (current > maxConsecutiveOnes) {
+                    maxConsecutiveOnes = current;
+                }
+            } else {
+                current = 0;
+            }
+        }
+        return maxConsecutiveOnes;
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Problem485FindMaxConsecutiveOnes p = new Problem485FindMaxConsecutiveOnes();
+        int[] nums = {1, 1, 0, 1, 1, 1, 1, 1, 1};
+        System.out.println(p.findMaxConsecutiveOnes(nums));
+    }
+}
