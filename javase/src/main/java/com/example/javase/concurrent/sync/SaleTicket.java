0a8dd257d25c761fe71445fbbefb4fd6c92d9838
ZPAVelocity <velocity120@outlook.com>
2022-07-19 01:02:14 +0800

    concurrent sync

---
+++ javase/src/main/java/com/example/javase/concurrent/sync/SaleTicket.java
@@ -0,0 +1,38 @@
+package com.example.javase.concurrent.sync;
+
+// 1、创建资源类，在资源类创建属性和操作方法
+// 2、创建多个线程，调用资源类的操作方法
+
+public class SaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                ticket.sale();
+            }
+
+        }, "saler1").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                ticket.sale();
+            }
+
+        }, "saler2").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                ticket.sale();
+            }
+
+        }, "saler3").start();
+    }
+}
+
+class Ticket {
+    private int num = 30;
+
+    public synchronized void sale() {
+        if (num > 0) {
+            System.out.println(Thread.currentThread().getName() + "卖票：" + num-- + "，剩余：" + num);
+        }
+    }
+}
