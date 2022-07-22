493c93360437434efa728bdd2cf2e30f9772ec4e
ZPAVelcoity <velocity120@outlook.com>
2022-07-22 15:16:38 +0800

    leetcode 119 easy 杨辉三角2

--- javase/src/main/java/com/example/javase/leetcode/Problem118YanghuiTriangleGenerate.java
+++ javase/src/main/java/com/example/javase/leetcode/Problem118YanghuiTriangleGenerate.java
@@ -3,6 +3,40 @@ package com.example.javase.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 给定一个非负整数numRows，生成「杨辉三角」的前numRows行。
+ * <p>
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例1:
+ * <p>
+ * <p>
+ * 输入:numRows=5
+ * 输出:[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ * <p>
+ * <p>
+ * 示例2:
+ * <p>
+ * <p>
+ * 输入:numRows=1
+ * 输出:[[1]]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * <p>
+ * 1<=numRows<=30
+ * <p>
+ * <p>
+ * RelatedTopics数组动态规划👍794👎0
+ */
+
 public class Problem118YanghuiTriangleGenerate {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
