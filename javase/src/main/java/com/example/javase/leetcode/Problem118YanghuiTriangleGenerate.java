f7dcd7263125de602d7a974cfe4e1b338e2d3d6d
ZPAVelcoity <velocity120@outlook.com>
2022-07-22 14:22:18 +0800

    leetcode 118 easy 杨辉三角

---
+++ javase/src/main/java/com/example/javase/leetcode/Problem118YanghuiTriangleGenerate.java
@@ -0,0 +1,26 @@
+package com.example.javase.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem118YanghuiTriangleGenerate {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
