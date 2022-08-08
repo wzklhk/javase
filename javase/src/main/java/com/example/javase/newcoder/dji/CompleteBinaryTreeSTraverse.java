2b2bcb1c954616cf08af40e77bca7d84c26af37d
zpavelocity <velocity120@outlook.com>
2022-08-08 16:52:37 +0800

    bytecode

---
+++ javase/src/main/java/com/example/javase/newcoder/dji/CompleteBinaryTreeSTraverse.java
@@ -0,0 +1,41 @@
+package com.example.javase.newcoder.dji;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompleteBinaryTreeSTraverse {
+
+    public static List<Integer> traverse(int[] tree) {
+        int length = tree.length;
+
+        List<Integer> result = new ArrayList<>();
+        boolean flag = true;
+
+        int layerNum = 0;
+
+        while ((int) Math.pow(2, layerNum) < length) {
+            if (flag) {
+                for (int i = (int) Math.pow(2, layerNum); i <= (int) Math.pow(2, layerNum + 1); i++) {
+                    result.add(tree[i]);
+                }
+            } else {
+                for (int i = (int) Math.pow(2, layerNum+1); i >= (int) Math.pow(2, layerNum); i--) {
+                    result.add(tree[i]);
+                }
+            }
+            flag = !flag;
+            layerNum++;
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] tree = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> traverse = traverse(tree);
+        System.out.println(traverse);
+    }
+
+
+}
