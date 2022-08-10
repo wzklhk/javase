a1e9fa4c3557fa2b2038b147c6a654af45849b20
zpavelocity <velocity120@outlook.com>
2022-08-10 17:51:30 +0800

    newcoder

---
+++ javase/src/main/java/com/example/javase/newcoder/bm/BM18BinarySearch2DimArray.java
@@ -0,0 +1,10 @@
+package com.example.javase.newcoder.bm;
+
+public class BM18BinarySearch2DimArray {
+    public boolean find(int target, int[][] array) {
+        if (array.length == 0) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ javase/src/main/java/com/example/javase/newcoder/sp/SP1ESReverseSort.java
@@ -0,0 +1,32 @@
+package com.example.javase.newcoder.sp;
+
+import java.util.ArrayList;
+
+/**
+ * 某云ES的文件管理系统中，文档由一个int型的ID和字符串类型的文档描述content组成，请根据提供的文档ID和文档描述，设计对搜索单词的倒排索引，根据输入要查找的单词，输出对应的文档ID。
+ * <p>
+ * 输入：[1, 5, 4, 9],["My lover", "Yours", "you are young", "My old age"],"My"
+ * <p>
+ * 返回值：[1,9]
+ */
+
+public class SP1ESReverseSort {
+    public ArrayList<Integer> invertedIndex(ArrayList<Integer> ID, ArrayList<String> content, String word) {
+        // write code here
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < content.size(); i++) {
+            if (content.get(i).contains(word)) {
+                result.add(ID.get(i));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "My lover";
+        String s2 = "My";
+        System.out.println(s1.contains(s2));
+    }
+}
