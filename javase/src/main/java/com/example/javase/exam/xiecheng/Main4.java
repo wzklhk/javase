c48ca95365c0e73d681bdb8ae4b0f648a9504308
zpavelocity <velocity120@outlook.com>
2022-08-31 15:28:26 +0800

    exam 携程

---
+++ javase/src/main/java/com/example/javase/exam/xiecheng/Main4.java
@@ -0,0 +1,43 @@
+package com.example.javase.exam.xiecheng;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+        int[] flatValue = flatValue(nums);
+        if (flatValue[1] == 0) {
+            nums[0] = nums[1];
+        } else if (flatValue[2] == n - 1) {
+            nums[n - 1] = nums[n - 2];
+        } else if ((nums[flatValue[2] + 1] - nums[flatValue[2]]) < (nums[flatValue[1]] - nums[flatValue[1] - 1])) {
+            int i = flatValue[2];
+            nums[i] = (nums[i - 1] + nums[i + 1]) / 2;
+        } else {
+            int i = flatValue[1];
+            nums[i] = (nums[i - 1] + nums[i + 1]) / 2;
+        }
+        flatValue = flatValue(nums);
+        System.out.println(flatValue[0]);
+    }
+
+    private static int[] flatValue(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[3];
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(nums[i] - nums[i - 1]) >= result[0]) {
+                result[0] = Math.abs(nums[i] - nums[i - 1]);
+                result[1] = i - 1;
+                result[2] = i;
+            }
+        }
+        return result;
+    }
+}
--- javase/src/main/java/com/example/javase/se/HashCodeAndEquals.java
+++ javase/src/main/java/com/example/javase/se/HashCodeAndEquals.java
@@ -10,13 +10,15 @@ public class HashCodeAndEquals {
 //        System.out.println(user1 == user2);
 //        System.out.println(user1.equals(user2));
 
-        int a = 10;
-        Integer b = 10;
-        Integer c = new Integer(10);
+//        int a = 10;
+//        Integer b = 10;
+//        Integer c = new Integer(10);
+//
+//        System.out.println(a == b);  // true
+//        System.out.println(b == c);  // false
+//        System.out.println(b.equals(c));  // true
 
-        System.out.println(a == b);  // true
-        System.out.println(b == c);  // false
-        System.out.println(b.equals(c));  // true
+        System.out.println(user1 instanceof Object);
     }
 }
 
