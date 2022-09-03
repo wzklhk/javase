6a7c48ece2a62dfaedf6f81cd6ea1180a3813643
zpavelocity <velocity120@outlook.com>
2022-09-03 09:40:07 +0800

    exam oppo

---
+++ javase/src/main/java/com/example/javase/exam/oppo/Solution.java
@@ -0,0 +1,21 @@
+package com.example.javase.exam.oppo;
+
+import java.util.Arrays;
+
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * @param digits int整型一维数组
+     * @return int整型
+     */
+    public int maxDigit(int[] digits) {
+        Arrays.sort(digits);
+        int result = 0;
+        for (int i = 0; i < digits.length; i++) {
+            result += (int) (digits[i] * Math.pow(10, i));
+        }
+        return result;
+    }
+}
