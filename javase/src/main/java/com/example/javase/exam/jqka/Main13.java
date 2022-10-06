33562526c704475fe91d6ff0167a5f6711b04b7d
ZPAVelocity <velocity120@outlook.com>
2022-10-06 18:58:50 +0800

    jqka

---
+++ javase/src/main/java/com/example/javase/exam/jqka/Main13.java
@@ -0,0 +1,48 @@
+package com.example.javase.exam.jqka;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main13 {
+    public int solve(int[] nums) {
+        int n2 = nums.length;
+        int n = n2 / 2;
+        Arrays.sort(nums);
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += nums[i];
+        }
+
+        return result;
+    }
+
+    public String filter(List<String> list) {
+        /*for (String str : list) {
+            if ("null".equals(str)) {
+                list.remove(str);
+            }
+        }
+*/
+        for (int i = 0; i < list.size(); i++) {
+            if ("null".equals(list.get(i))) {
+                list.remove(list.get(i));
+            }
+        }
+        return list.get(0);
+    }
+
+
+    public static void main(String[] args) {
+        Main13 p = new Main13();
+
+        List<String> list = new ArrayList<>();
+
+        list.add("null");
+        list.add("ths");
+        list.add("null");
+
+        System.out.println(p.filter(list));
+    }
+}
