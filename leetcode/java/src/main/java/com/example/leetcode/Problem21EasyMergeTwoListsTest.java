1699c1a9178c98237f72e130aacf59b5f5eb2b0a
ZPAVelocity <velocity120@outlook.com>
2023-08-05 14:35:45 +0800

    leetcode 21 easy 合并两个有序

--- leetcode/java/src/main/java/com/example/leetcode/Problem21EasyMergeTwoListsTest.java
+++ leetcode/java/src/main/java/com/example/leetcode/Problem21EasyMergeTwoListsTest.java
@@ -6,7 +6,49 @@ import org.junit.Test;
 /**
  * @author wzklhk
  * @date 2022/11/7 9:53
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ * <p>
+ * <p>
+ * Related Topics 递归 链表 👍 3212 👎 0
+ * <p>
+ * <p>
+ * leetcode submit region begin(Prohibit modification and deletion)
  */
+
+
 public class Problem21EasyMergeTwoListsTest {
     public ListNode mergeTwoListsIter(ListNode list1, ListNode list2) {
         ListNode l1 = list1;
