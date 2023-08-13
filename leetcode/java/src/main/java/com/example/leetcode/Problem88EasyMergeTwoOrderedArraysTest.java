7ff8809a9bd5d07b41a3930dd0a1644efbe95392
ZPAVelocity <velocity120@outlook.com>
2023-08-13 19:28:56 +0800

    leetcode 88 easy 合并两个有序数组

---
+++ leetcode/java/src/main/java/com/example/leetcode/Problem88EasyMergeTwoOrderedArraysTest.java
@@ -0,0 +1,140 @@
+package com.example.leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author wzklhk
+ * <p>
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * <p>
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * <p>
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并
+ * 的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+ * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ * 解释：需要合并 [1] 和 [] 。
+ * 合并结果是 [1] 。
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+ * 输出：[1]
+ * 解释：需要合并的数组是 [] 和 [1] 。
+ * 合并结果是 [1] 。
+ * 注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -10⁹ <= nums1[i], nums2[j] <= 10⁹
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
+ * <p>
+ * Related Topics 数组 双指针 排序 👍 2000 👎 0
+ */
+
+
+public class Problem88EasyMergeTwoOrderedArraysTest {
+    public void mergeSort(int[] nums1, int m, int[] nums2, int n) {
+        /*
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+        */
+        System.arraycopy(nums2, 0, nums1, m, nums2.length);
+        Arrays.sort(nums1);
+    }
+
+    public void mergeDoublePoints(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0;
+        int p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2];
+                p2++;
+            } else if (p2 == n) {
+                cur = nums1[p1];
+                p1++;
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1];
+                p1++;
+            } else {
+                cur = nums2[p2];
+                p2++;
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = sorted[i];
+        }
+    }
+
+    public void mergeDoublePointsReverse(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2];
+                p2--;
+            } else if (p2 == -1) {
+                cur = nums1[p1];
+                p1--;
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1];
+                p1--;
+            } else {
+                cur = nums2[p2];
+                p2--;
+            }
+            nums1[tail] = cur;
+            tail--;
+        }
+    }
+
+    @Test
+    public void test() {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        mergeDoublePointsReverse(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+}
